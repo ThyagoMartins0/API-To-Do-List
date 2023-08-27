502fbd74b93be02714e5dccf4fa4fda8a09f3dee
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2023-08-27 19:30:05 -0300

    primeiro commit

--- src/main/java/com/code/todolist/controller/TaskController.java
+++ src/main/java/com/code/todolist/controller/TaskController.java
@@ -3,11 +3,15 @@ package com.code.todolist.controller;
 
 import com.code.todolist.service.TaskService;
 import com.code.todolist.model.Task;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -19,6 +23,13 @@ public class TaskController {
 
     TaskService taskService;
 
+   //primeiro ENDPOINT
+   @ApiOperation(value = "Create a task")
+           @ApiResponses(value = {
+                   @ApiResponse(code = 201, message = "Task created"),
+                   @ApiResponse(code = 400, message = "Invalid request"),
+                   @ApiResponse(code = 500, message = "Internal server error")
+           })
 
     @PostMapping("/tasks")
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,28 +37,52 @@ public class TaskController {
     { log.info("Task created[{}]",task);
         return taskService.createTask(task);
     }
+    //SEGUNDO ENDPOINT
+    @ApiOperation(value = "Get all tasks")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Task found"),
+    })
 
-    @GetMapping("/tasks")
+            @GetMapping("/tasks")
     @ResponseStatus(HttpStatus.OK)
     public List<Task> getAllTasks() {
         return taskService.listAllTasks();
     }
 
-    @GetMapping("/tasks/{id}")// {id} is a path variable
+    //TERCEIRO ENDPOINT
+    @ApiOperation(value = "Get a task by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Task found"),
+    })
+
+            @GetMapping("/tasks/{id}")// {id} is a path variable
     @ResponseStatus(HttpStatus.OK)// this annotation is used to set the http status code
     public ResponseEntity<Task> getTaskById(@PathVariable (value = "id") Long id)// @PathVariable annotation is used to bind the path variable with a method parameter.
      { log.info("Task found[{}]",id);
         return taskService.findTaskById(id);
     }
 
+    //QUARTO ENDPOINT
+    @ApiOperation(value = "Update a task by id")
+        @ApiResponses(value = {
+                @ApiResponse(code = 200, message = "Task updated"),
+        })
 
-    @PutMapping  ("/tasks/{id}")
+
+            @PutMapping  ("/tasks/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Task> UpdateById(@PathVariable (value = "id")Long id, @RequestBody Task task)
     {log.info("Task updated[{}]",id);
         return taskService.updateTaskId(task,id);
     }
-    @DeleteMapping("/tasks/{id}")
+
+    //QUINTO ENDPOINT
+    @ApiOperation(value = "Delete a task by id")
+        @ApiResponses(value = {
+                @ApiResponse(code = 204, message = "Task deleted") })
+
+
+            @DeleteMapping("/tasks/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Object> DeleteById(@PathVariable (value = "id") Long id)
     {log.info("Task deleted[{}]",id);
